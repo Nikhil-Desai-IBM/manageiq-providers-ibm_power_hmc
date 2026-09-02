5388ca2a7c5e9e49b71fce45f36ab091d70077ba
Siddhant Pawar <siddhantpawar1203@gmail.com>
2026-09-02 16:02:52 +0530

    fix(sleep): added poll sleep
    Signed-off-by: Siddhant Pawar <siddhantpawar1203@gmail.com>

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/event_catcher/stream.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/event_catcher/stream.rb
@@ -1,8 +1,9 @@
 class ManageIQ::Providers::IbmPowerHmc::InfraManager::EventCatcher::Stream
-  def initialize(ems, _options = {})
+  def initialize(ems, options = {})
     @ems = ems
     @last_activity = nil
     @stop_polling = false
+    @options = options
   end
 
   def start
@@ -16,9 +17,14 @@ class ManageIQ::Providers::IbmPowerHmc::InfraManager::EventCatcher::Stream
   def poll(&block)
     @ems.with_provider_connection do |connection|
       # The HMC waits 10 seconds by default before returning if there is no event.
-      connection.next_events(false).select do |event|
-        event.type.in?(["ADD_URI", "MODIFY_URI", "DELETE_URI"])
-      end.each(&block) until @stop_polling
+      until @stop_polling
+        connection.next_events(false).select do |event|
+          event.type.in?(["ADD_URI", "MODIFY_URI", "DELETE_URI"])
+        end.each do |event|
+          block.call(event)
+        end
+        sleep(@options[:poll_sleep])
+      end
     rescue IbmPowerHmc::Connection::HttpError => e
       $ibm_power_hmc_log.error("querying hmc events failed: #{e}")
       raise
